1e64abb447206f8625ec29bb59faa8a2c0ec273a
Ralence <ranko.ostojic@gmail.com>
2019-09-03 16:26:11 +0800

    feat: add new BookContext.js change BookList.js, NavBar.js and ThemeToggle.js to functional components add new README.md

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BookList from './components/BookList/BookList';
 import ThemeContextProvider from './context/ThemeContext';
 import ThemeToggler from './components/ThemeToggle/ThemeToggle';
 import AuthContextProvider from './context/AuthContext';
+import BookContextProvider from './context/BookContext';
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
       <ThemeContextProvider>
         <AuthContextProvider>
           <NavBar />
-          <BookList />
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
           <ThemeToggler />
         </AuthContextProvider>
       </ThemeContextProvider>
--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../../context/ThemeContext';
+import { BookContext } from '../../context/BookContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div className="book-list" style={{ background: theme.bg, color: theme.text }}>
-        <ul>
-          <li style={{ background: theme.ui }}>the way of kings</li>
-          <li style={{ background: theme.ui }}>the name of the wind</li>
-          <li style={{ background: theme.ui }}>the final empire</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div className="book-list" style={{ background: theme.bg, color: theme.text }}>
+      <ul>
+        {books.map(book => (
+          <li key={book.id} style={{ background: theme.ui }}>
+            {book.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
